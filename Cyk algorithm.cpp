46ad6a96c02c7be1787ba6b41660e683b35a4bcd
remus.arthur <remus.arthur@gmail.com>
2015-04-22 16:14:11 +0300

    print CFG trace for acceptance

--- Cyk algorithm.cpp
+++ Cyk algorithm.cpp
@@ -14,19 +14,46 @@ using namespace std;
 const int NMAX = 50;
 
 map<string, vector<char> > reverseCNF;
-vector<tuple<char /*letter*/, tuple<int,int,int> /*leftSon*/, tuple<int,int,int> /*rightSon*/> > Cyk[NMAX][NMAX];
+vector<tuple<char /*letter*/, tuple<int, int, int> /*leftSon*/, tuple<int, int, int> /*rightSon*/> > Cyk[NMAX][NMAX];
+ifstream in("cyk.txt");
+ofstream out("cykRes.txt");
+string word;
 
+void Decode(int x, int y, int cellElem)
+{
+	if (x == 0 || y == 0)
+	{
+		out << "\"" << word[cellElem - 1] << "\"";
+		return;
+	}
+	int lx, ly, li, rx, ry, ri;
+	lx = get<0>(get<1>(Cyk[x][y][cellElem]));
+	ly = get<1>(get<1>(Cyk[x][y][cellElem]));
+	li = get<2>(get<1>(Cyk[x][y][cellElem]));
+
+	rx = get<0>(get<2>(Cyk[x][y][cellElem]));
+	ry = get<1>(get<2>(Cyk[x][y][cellElem]));
+	ri = get<2>(get<2>(Cyk[x][y][cellElem]));
+
+	char letter = get<0>(Cyk[x][y][cellElem]);
+
+	//out << letter << '\n';
+	out << "\"" << letter << "\" : {\n";
+	Decode(lx, ly, li);
+	out << ",\n";
+	Decode(rx, ry, ri);
+	out << "\n}";
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 
-	ifstream in("cyk.in");
-	ofstream out("cyk.out");
-	string to, word;
+
+	string to;
 	char from;
 	in >> word;
-	int N = word.size();
-	while (in>>from >> to)
+	int N = static_cast<int>(word.size());
+	while (in >> from >> to)
 	{
 		reverseCNF[to].push_back(from);
 	}
@@ -35,15 +62,15 @@ int _tmain(int argc, _TCHAR* argv[])
 	for (int wordIndex = 1; wordIndex <= N; wordIndex++)
 	{
 		string letter = "";
-		letter += word[wordIndex-1];
-		
+		letter += word[wordIndex - 1];
+
 		for (int revState = 0; revState < reverseCNF[letter].size(); revState++)
 		{
-			Cyk[N][wordIndex].push_back(make_tuple(reverseCNF[letter][revState], make_tuple(0,0,0), make_tuple(0,0,0)));
+			Cyk[N][wordIndex].push_back(make_tuple(reverseCNF[letter][revState], make_tuple(0, 0, wordIndex), make_tuple(0, 0, wordIndex)));
 		}
 	}
 
-	
+
 	for (int row = N - 1; row >= 1; row--)
 	{
 		for (int column = 1; column <= row; column++)
@@ -63,7 +90,7 @@ int _tmain(int argc, _TCHAR* argv[])
 
 						for (int revState = 0; revState < reverseCNF[concLetters].size(); revState++)
 						{
-							Cyk[row][column].push_back(make_tuple(reverseCNF[concLetters][revState], make_tuple(row + level, column,cellElem), make_tuple(N - level + 1, column + (N - level + 1 - row),neighbourElem)));
+							Cyk[row][column].push_back(make_tuple(reverseCNF[concLetters][revState], make_tuple(row + level, column, cellElem), make_tuple(N - level + 1, column + (N - level + 1 - row), neighbourElem)));
 
 						}
 					}
@@ -71,9 +98,34 @@ int _tmain(int argc, _TCHAR* argv[])
 			}
 		}
 
-		//row = -1;
 	}
 
+	for (int cellElem = 0; cellElem < Cyk[1][1].size(); cellElem++)
+	{
+		char letter1 = get<0>(Cyk[1][1][cellElem]);
+		if (letter1 == 'S')
+		{
+			out << "Possible!\n";
+			out << "\"S\":{ ";
+
+
+			int lx, ly, li, rx, ry, ri;
+			lx = get<0>(get<1>(Cyk[1][1][cellElem]));
+			ly = get<1>(get<1>(Cyk[1][1][cellElem]));
+			li = get<2>(get<1>(Cyk[1][1][cellElem]));
+
+			rx = get<0>(get<2>(Cyk[1][1][cellElem]));
+			ry = get<1>(get<2>(Cyk[1][1][cellElem]));
+			ri = get<2>(get<2>(Cyk[1][1][cellElem]));
+
+			Decode(lx, ly, li);
+			out << ",";
+			Decode(rx, ry, ri);
+			out << "}\n\n\n";
+
+			break;
+		}
+	}
 
 	for (int row = 1; row <= N; row++)
 	{
@@ -91,4 +143,3 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	return 0;
 }
-
